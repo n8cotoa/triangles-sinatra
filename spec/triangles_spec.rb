a2f4f4b7f3a62d3e8995036de804b0dbe06a2473
Nate Cottle and Reese Glasscock <reese@>
2018-07-02 09:33:52 -0700

    Add first positive pass.

--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -4,7 +4,7 @@ require('triangles')
 
 describe('#triangles') do
   it('Program will check if input is not a triangle.') do
-    triangle = Triangle.new(2, 2, 1)
+    triangle = Triangle.new(2, 2, 10)
     expect(triangle.triangle?).to(eq("not a triangle"))
   end
 end
