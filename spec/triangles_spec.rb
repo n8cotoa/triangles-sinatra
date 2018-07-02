96c48af3eb49ced6225530cb59530cffb543f95c
Nate Cottle and Reese Glasscock <nate@>
2018-07-02 09:35:36 -0700

    second positive fail.

--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -7,4 +7,8 @@ describe('#triangles') do
     triangle = Triangle.new(2, 2, 10)
     expect(triangle.triangle?).to(eq("not a triangle"))
   end
+  it('Program will check if input is a equilateral triangle.') do
+    triangle = Triangle.new(2, 2, 2)
+    expect(triangle.triangle?).to(eq("Equilateral"))
+  end
 end
