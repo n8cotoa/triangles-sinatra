0757b665c178efe1104efe64fd43ad57280f401a
Nate Cottle and Reese Glasscock <nate@>
2018-07-02 09:33:24 -0700

    add first positive fail.

---
+++ lib/triangles.rb
@@ -0,0 +1,14 @@
+class Triangle
+  def initialize(length1, length2, length3)
+    @l1 = length1
+    @l2 = length2
+    @l3 = length3
+  end
+
+  def triangle?()
+    if @l1 + @l2 <= @l3 || @l2 + @l3 <= @l1 || @l3 + @l1 <= @l2
+      'not a triangle'
+    end
+  end
+
+end
---
+++ spec/triangles_spec.rb
@@ -0,0 +1,10 @@
+require('rspec')
+require('pry')
+require('triangles')
+
+describe('#triangles') do
+  it('Program will check if input is not a triangle.') do
+    triangle = Triangle.new(2, 2, 1)
+    expect(triangle.triangle?).to(eq("not a triangle"))
+  end
+end
